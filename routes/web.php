73116b09cbf2d2a2d19822ad463f62198e935871
WnRyno <wnaida05@gmail.com>
2023-06-03 17:27:40 +0800

    userprofile

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserProfileController;
+use App\Models\UserProfile;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/UserProfile',function(){
+    return 'bitch you better not';
+})->name('UserProfile.index');
